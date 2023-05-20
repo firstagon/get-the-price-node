d14bea087f009bd2d6d97f923bd86e40e09d06a7
firstagon <98339053+firstagon@users.noreply.github.com>
2023-05-20 15:19:45 +0500

    added kostil for updating existing price in array's price

---
+++ models/updateDB/updateItem.js
@@ -0,0 +1,89 @@
+const getDb = require("../../db/mongo").getDb;
+
+exports.updateData = (samePrice, data, sesId) => {
+    const db = getDb().db("main").collection("users");
+  if (samePrice) {
+    db.updateOne(
+      { lastSessionId: sesId },
+      {
+        $set: {
+          "userData.$[a].lastPrice": data.itemPrice,
+          "userData.$[a].updated": new Date().toLocaleString(),
+          "userData.$[a].data.itemRating": data.itemRating,
+        },
+        $pop: { "userData.$[a].data.itemPrice": 1 },
+      },
+      { arrayFilters: [{ "a.itemCode": data.itemCode }] }
+    )
+      .then(() => {
+        db.updateOne(
+          { lastSessionId: sesId },
+          {
+            $push: { "userData.$[a].data.itemPrice": { price: data.itemPrice, updated: new Date().toLocaleString() } },
+          },
+          { arrayFilters: [{ "a.itemCode": data.itemCode }] }
+        );
+      })
+      // .then((res) => console.log("writed same price" + " " + data.itemName + data.itemCode))
+      .catch((err) => {
+        throw err;
+      });
+  } else {
+    db.updateOne(
+      { lastSessionId: sesId },
+      {
+        $set: {
+          "userData.$[a].lastPrice": data.itemPrice,
+          "userData.$[a].updated": new Date().toLocaleString(),
+          "userData.$[a].data.itemRating": data.itemRating,
+        },
+        $push: { "userData.$[a].data.itemPrice": { price: data.itemPrice, updated: new Date().toLocaleString() } },
+      },
+      { arrayFilters: [{ "a.itemCode": data.itemCode }] }
+    )
+      // .then((rs) => console.log("writed not same" + " " + data.itemName + data.itemCode))
+      .catch((err) => {
+        throw err;
+      });
+  }
+};
+
+// db.updateOne(
+//   { lastSessionId: sesId },
+//   {
+//     $set: {
+//       "userData.$[a].lastPrice": data.itemPrice,
+//       "userData.$[a].updated": new Date().toLocaleString(),
+//       "userData.$[a].data.itemRating": data.itemRating,
+//     },
+//   },
+//   { arrayFilters: [{ "a.itemCode": data.itemCode }] },
+//   { $push: { "userData.$[a].data.itemPrice": { price: data.itemPrice, data: new Date().toLocaleString() } } },
+//   { arrayFilters: [{ "a.itemCode": data.itemCode }] }
+// )
+//   .then()
+//   .catch((err) => {
+//     throw err;
+//   });
+
+// db.updateOne(
+//   { lastSessionId: sesId },
+//   {
+//     $addToSet: {
+//       userData: {
+//         itemCode: data.itemCode,
+//         itemName: data.itemName,
+//         lastPrice: data.itemPrice,
+//         updated: new Date().toLocaleString(),
+//         data: {
+//           ...data,
+//           itemPrice: [{ ...data.itemPrice, price: data.itemPrice, updated: new Date().toLocaleString() }],
+//         },
+//       },
+//     },
+//   }
+// )
+//   .then()
+//   .catch((err) => {
+//     throw err;
+//   });
--- models/writeData.js
+++ models/writeData.js
@@ -2,11 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const mongoConnect = require("../db/mongo");
 const getDb = require("../db/mongo").getDb;
+const { updateData } = require("./updateDB/updateItem");
 
 const p = path.join(path.dirname(process.mainModule.filename), "../", "/data/users.json");
 
 const samePrice = (array, currPrice, itemCode) => {
-  console.log(array, currPrice);
+  // console.log(array, currPrice);
   let check = false;
   for (val of array) {
     if (val.itemCode === itemCode) {
@@ -18,45 +19,15 @@ const samePrice = (array, currPrice, itemCode) => {
   return check;
 };
 
-const priceHandler = (prev, val) => {
-  // console.log(prev, val);
-  // console.log("typeof" + typeof(arr))
-  if (prev === val) {
-    return (samePrevPrice = true);
-  } else {
-    return (samePrevPrice = false);
-  }
-};
-
-let samePrevPrice = null;
-// const client = getDb();
-
 const writeData = async (data, sesId) => {
   const db = getDb().db("main").collection("users");
 
   db.findOne({ lastSessionId: sesId, userData: { $elemMatch: { itemCode: data.itemCode } } }).then((res) => {
     // console.log(res.userData)
     const same = samePrice(res.userData, data.itemPrice, data.itemCode);
-
     const _itemExist = res ? true : false;
     if (_itemExist) {
-      db.updateOne(
-        { lastSessionId: sesId },
-        {
-          $set: {
-            "userData.$[a].lastPrice": data.itemPrice,
-            "userData.$[a].updated": new Date().toLocaleString(),
-            "userData.$[a].data.itemRating": data.itemRating,
-          },
-        },
-        { arrayFilters: [{ "a.itemCode": data.itemCode }] },
-        { $push: { "userData.$[a].data.itemPrice": { price: data.itemPrice, data: new Date().toLocaleString() } } },
-        { arrayFilters: [{ "a.itemCode": data.itemCode }] }
-      )
-        .then()
-        .catch((err) => {
-          throw err;
-        });
+      updateData(!!same, data, sesId);
     } else {
       db.updateOne(
         { lastSessionId: sesId },
@@ -81,117 +52,6 @@ const writeData = async (data, sesId) => {
         });
     }
   });
-
-  // db.updateOne(
-  //   { lastSessionId: sesId },
-  //   { $push: { "userData.$[a].data.itemPrice": { price: data.itemPrice, data: new Date().toLocaleString() } } },
-  //   { arrayFilters: [{ "a.itemCode": data.itemCode }] }
-  // ).then((resp) => console.log(resp));
-
-  // db.findOne({ lastSessionId: sesId }, { userData: { $elemMatch: { itemCode: data.itemCode } } }).then((resp) => {
-  //   console.log(resp.userData);
-  //   for (val of resp.userData) {
-  //     if (val.itemCode === data.itemCode) {
-  //       console.log("TRUE")
-  //       return
-  //     }
-  //   }
-  // });
-};
-
-const writeData_1 = (data, sesId) => {
-  // console.log("WRITEDATA.JS -> SES ID " + sesId);
-  const db = getDb().db("main").collection("users");
-  // console.log(db);
-
-  // db.updateOne(
-  //   { "userData.itemCode": data.itemCode },
-  //   { $push: { "userData.$.data.itemPrice": { price: data.itemPrice, updated: new Date().toLocaleString() } } }
-  // )
-
-  // db.updateOne(
-  //   { lastSessionId: sesId, userData: { $elemMatch: { itemCode: data.itemCode } } },
-  //   { $addToSet: { userData: {itemCode: data.itemCode, itemName: data.itemName, price: data.itemPrice, data: data} } }
-  // );
-  // db.findOne({ lastSessionId: sesId, userData: { $elemMatch: { itemCode: data.itemCode } } })
-
-  db.findOne({ lastSessionId: sesId }, { userData: { $elemMatch: { itemCode: data.itemCode } } })
-    .then((resp) => {
-      console.log(resp.userData);
-      const exist = resp ? true : false;
-      if (exist) {
-        console.log("EXIST");
-        // const respPrice = resp.userData[0].data.itemPrice[0].price;
-        // console.log(respPrice)
-        // priceHandler(respPrice, data.itemPrice);
-        if (samePrevPrice) {
-          console.log("TRUE MTFK " + samePrevPrice);
-          // db.find({ lastSessionId: sesId, userData: { itemCode: { elemMatch: data.itemCode } } })
-          //   .then((resp = console.log(resp.userData)))
-          //   .catch((err) => console.warn(err));
-
-          db.updateOne(
-            { lastSessionId: sesId, userData: { $elemMatch: { itemCode: data.itemCode } } },
-            {
-              $set: {
-                userData: {
-                  data: {
-                    itemPrice: [{ price: data.price, date: new Date().toLocaleString() }],
-                  },
-                },
-              },
-            }
-          )
-            .then((resp) => console.log("writed TRUE MTHFK"))
-            .catch((err) => console.log(err));
-        } else {
-          console.log("FALSE MTHFK " + samePrevPrice);
-          db.updateOne(
-            { lastSessionId: sesId, userData: { $elemMatch: { "itemCode.itemCode": data.itemCode } } },
-            {
-              $addToSet: {
-                userData: {
-                  itemCode: data.itemCode,
-                  itemName: data.itemName,
-                  lastPrice: data.itemPrice,
-                  updated: new Date().toLocaleString(),
-                  data: {
-                    ...data,
-                    itemPrice: [{ ...data.itemPrice, price: data.itemPrice, updated: new Date().toLocaleString() }],
-                  },
-                },
-              },
-            }
-          )
-            .then((resp) => console.log("writed"))
-            .catch((err) => console.log(err));
-        }
-      } else {
-        console.log("NOT EXIST");
-        db.updateOne(
-          { lastSessionId: sesId },
-          {
-            $addToSet: {
-              userData: {
-                itemCode: data.itemCode,
-                itemName: data.itemName,
-                lastPrice: data.itemPrice,
-                updated: new Date().toLocaleString(),
-                data: { ...data, itemPrice: [{ price: data.itemPrice, updated: new Date().toLocaleString() }] },
-              },
-            },
-          }
-        )
-          .then((resp) => console.log("writed"))
-          .catch((err) => console.log(err));
-      }
-    })
-    .then((resp) => console.log("WRITEDATA.js -> writed"))
-    .catch((err) => console.log("WRITEDATA.js -> err in find " + err));
-};
-
-const checkDuplicates = (data) => {};
-
-// writeData()
+}
 
 module.exports = writeData;
