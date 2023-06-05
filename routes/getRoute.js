45a6145b075d56c0cf94e8571dd89acb99856f17
firstagon <98339053+firstagon@users.noreply.github.com>
2023-06-05 18:48:17 +0500

    added jws authentication on all pages

--- routes/getRoute.js
+++ routes/getRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const setUrl = require("../controllers/postUrl");
 const getHome = require("../controllers/getHome");
+const isAuth = require("../middleware/isAuth");
 
-router.get("/", getHome.getHome);
+router.get("/", isAuth, getHome.getHome);
 
-router.post("/", setUrl.incomingURL);
+router.post("/", isAuth, setUrl.incomingURL);
 
 module.exports = router;
