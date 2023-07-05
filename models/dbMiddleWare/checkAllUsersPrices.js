87861c6d7bad1fd6c15e40a134f73fd3eedcebd2
firstagon <98339053+firstagon@users.noreply.github.com>
2023-07-05 21:38:59 +0500

    added query for updating items

--- models/dbMiddleWare/checkAllUsersPrices.js
+++ models/dbMiddleWare/checkAllUsersPrices.js
@@ -8,24 +8,53 @@ const p = path.join(path.dirname(process.mainModule.filename), "../../", "/data/
 
 let _items = [];
 
-const sort = (arr) => {
-    const id = arr._id.toString();
-    arr.userData.forEach(el => {
-        
-    });
-}
+const setItemsQuery = (arr) => {
+  // console.log(arr);
+
+  // const id = arr._id.toString();
+
+  const codesDeck = [];
+  const itemsDeck = [];
+
+  // creating deck with item codes
+  arr.forEach((el) => {
+    if (el.userData.length === 0) {
+      return;
+    }
+
+    for (i = 0; i < el.userData.length; i++) {
+      if (!codesDeck.includes(el.userData[i].itemCode)) {
+        codesDeck.push(el.userData[i].itemCode);
+        itemsDeck.push({ ...el.userData[i], query: [el._id.toString()] });
+      } else {
+        for (j = 0; j < itemsDeck.length; j++) {
+          if (itemsDeck[j].itemCode == el.userData[i].itemCode) {
+            itemsDeck[j] = { ...itemsDeck[j], query: [...itemsDeck[j].query, el._id.toString()] };
+          }
+        }
+      }
+    }
+  });
+
+  // console.log(itemsDeck);
+  return itemsDeck;
+};
 
 const checkUsersPrices = async () => {
   const db = getDb().db("main").collection("users");
   const unCheckedItems = [];
   const allUsers = await db.find().toArray();
-  console.log("working");
-  console.log(allUsers);
-  fs.writeFile(p, JSON.stringify(allUsers), err => console.log(err));
+  // console.log("working");
+  // console.log(allUsers);
+  const itemsQuery = setItemsQuery(allUsers);
+  // console.log( itemsQuery)
+  // fs.writeFile(p, JSON.stringify(allUsers), err => console.log(err));
+
+  
 };
 
 mongoConnect(() => {
-  console.log("connected!");
+  // console.log("connected!");
   checkUsersPrices();
 });
 
